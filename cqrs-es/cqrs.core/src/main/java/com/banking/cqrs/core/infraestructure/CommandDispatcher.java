37f8a66e0178763e8db58677db229efb7c230ad8
Jose Funez <joseafp@algartech.com>
2023-05-21 19:40:59 -0500

    CQRS project Added

---
+++ cqrs-es/cqrs.core/src/main/java/com/banking/cqrs/core/infraestructure/CommandDispatcher.java
@@ -0,0 +1,10 @@
+package com.banking.cqrs.core.infraestructure;
+
+
+import com.banking.cqrs.core.commands.BaseCommand;
+import com.banking.cqrs.core.commands.CommandHandlerMethod;
+
+public interface CommandDispatcher {
+    <T extends BaseCommand> void registerHandler(Class<T> type, CommandHandlerMethod<T> handler);
+    void send(BaseCommand command);
+}
